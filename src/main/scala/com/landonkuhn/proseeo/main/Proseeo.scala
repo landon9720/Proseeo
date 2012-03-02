0748f35d9cb730f2c91605eaaac416949947031e
Landon Kuhn <landon9720@gmail.com>
2012-03-02 03:19:13 -0800

    parser

--- src/main/scala/com/landonkuhn/proseeo/main/Proseeo.scala
+++ src/main/scala/com/landonkuhn/proseeo/main/Proseeo.scala
@@ -1,8 +1,28 @@
 package com.landonkuhn.proseeo.main
 
+import util.parsing.combinator.RegexParsers
+
 object Proseeo {
   def main(args: Array[String]) {
-    println("Hello, world")
-    println(args.mkString("-"))
+    println(parseCommandLine(args.mkString("-")))
+  }
+
+  def parseCommandLine(args: String): Command = parser.parseAll(parser.command, args) match {
+    case parser.Success(command, _) => command
+    case e: parser.NoSuccess => Error("Failed parsing [%s]: %s".format(args, e.msg))
+  }
+
+  private val parser = new RegexParsers {
+
+    private implicit def string_to_regex(s: String) = s.r
+
+    def command = regex("help") ^^ {
+      case _ => Help()
+    }
   }
 }
+
+trait Command
+
+case class Error(message: String) extends Command
+case class Help() extends Command
